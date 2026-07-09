d9391431cdc21bd8fd61af38629d57eb5bb0e2b0
Ahmed Amy's <ahmedamys337@gmail.com>
2026-07-10 02:08:38 +0500

    UX: Disable all ad scripts temporarily to identify redirect source
    - Disabled all three ad scripts to test which one causes redirect
    - Will re-enable non-redirecting ads after testing
    - This will help identify the exact ad causing the bad first-time user experience

--- src/components/AdsterraAds.tsx
+++ src/components/AdsterraAds.tsx
@@ -97,6 +97,7 @@ export default function AdsterraAds() {
 
   return (
     <>
+      {/* TEMPORARILY DISABLED: Testing which ad causes redirect
       <Script
         src="https://pl30087661.effectivecpmnetwork.com/7e/da/3a/7eda3aedc8736e42270e448f7f5531a6.js"
         strategy="afterInteractive"
@@ -105,6 +106,7 @@ export default function AdsterraAds() {
         src="https://pl30087662.effectivecpmnetwork.com/65/f9/20/65f920d880821ea5435bf95e085e0b30.js"
         strategy="afterInteractive"
       />
+      */}
       {/* TEMPORARILY DISABLED: Redirecting ad causing bad first-time user experience
       <div id="container-5bc8ac44c319f4229556e677c20a528d" className="min-h-[90px] min-w-[320px] max-w-[728px] w-full mx-auto overflow-hidden" />
       <Script
